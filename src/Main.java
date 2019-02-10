b3a0258265df57e872e31164383a0cf3effc9be1
mitodz <mitodzzz@gmail.com>
2019-02-10 14:29:38 +0300

    Perhaps it works!

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,26 @@
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Queue<Integer> q = new PriorityQueue<>(Comparator.reverseOrder());
+        Scanner scanner= new Scanner("6\n" +
+                                            "Insert 200\n" +
+                                            "Insert 10\n" +
+                                            "ExtractMax\n" +
+                                            "Insert 5\n" +
+                                            "Insert 500\n" +
+                                            "ExtractMax").useDelimiter("\\W");
+        int n = scanner.nextInt(); // число операций с очередью
+        while (scanner.hasNext()) {
+            if (scanner.next().equalsIgnoreCase("Insert")) {
+                q.add(scanner.nextInt());
+            } else {
+                System.out.println(q.poll());
+            }
+        }
     }
 }
