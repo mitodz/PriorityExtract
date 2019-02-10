3cd4054972513cfb7e0abd5d25bfa93670f7af2d
mitodz <mitodzzz@gmail.com>
2019-02-10 14:31:29 +0300

    it works!

--- src/Main.java
+++ src/Main.java
@@ -7,13 +7,7 @@ public class Main {
 
     public static void main(String[] args) {
         Queue<Integer> q = new PriorityQueue<>(Comparator.reverseOrder());
-        Scanner scanner= new Scanner("6\n" +
-                                            "Insert 200\n" +
-                                            "Insert 10\n" +
-                                            "ExtractMax\n" +
-                                            "Insert 5\n" +
-                                            "Insert 500\n" +
-                                            "ExtractMax").useDelimiter("\\W");
+        Scanner scanner= new Scanner(System.in).useDelimiter("\\W");
         int n = scanner.nextInt(); // число операций с очередью
         while (scanner.hasNext()) {
             if (scanner.next().equalsIgnoreCase("Insert")) {
